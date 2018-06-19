3cdb2bdb404a44623807c3b6194c0780d10978d5
Matt Arnold <marnold@gospotcheck.com>
2018-06-19 12:50:48 -0600

    Styles prior to eject

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ class App extends Component {
   deletePersonHandler = personIndex => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+    this.setState({ persons: persons });
+  };
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
 
-    const person = {...this.state.persons[personIndex]}
+    const person = { ...this.state.persons[personIndex] };
 
     person.name = event.target.value;
 
@@ -42,7 +42,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -55,22 +56,35 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person 
+              <Person
                 key={person.id}
-                click={() => { this.deletePersonHandler(index)}}
-                name={person.name} 
-                age={person.age} 
-                changed={(event) => {this.nameChangedHandler(event, person.id)}}  
+                click={() => {
+                  this.deletePersonHandler(index);
+                }}
+                name={person.name}
+                age={person.age}
+                changed={event => {
+                  this.nameChangedHandler(event, person.id);
+                }}
               />
             );
           })}
         </div>
       );
+      style.backgroundColor = "red";
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
     return (
       <div className="App">
         <h1>Hi, I'm a react app</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(" ")}>This is really working!</p>
         <button style={style} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
