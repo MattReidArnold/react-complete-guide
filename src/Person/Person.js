a7d947bb8608920339b25e6186c528d46cd11874
Matt Arnold <marnold@gospotcheck.com>
2018-06-20 20:07:24 -0600

    Error Boundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import classes from './Person.css';
+import React from "react";
+import classes from "./Person.css";
 
-const person = (props) => {
-    return (
-        <div className={classes.Person}>
-            <p onClick={() => props.click(props.name)}>I'm {props.name} and i am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    );
-}
+const person = props => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={() => props.click(props.name)}>
+        I'm {props.name} and i am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
 
 export default person;
